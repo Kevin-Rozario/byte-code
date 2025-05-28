88b3199f46de70d0716e9694933e0aa919067793
Kevin Rozario <rozariokevin73@gmail.com>
2025-05-28 10:10:47 +0530

    refactor: remove unused authentication check and navigation in SolveProblemPage

--- frontend/src/pages/SolveProblemPage.tsx
+++ frontend/src/pages/SolveProblemPage.tsx
@@ -46,12 +46,11 @@ import { Route } from "@/routes/problems/problem.$id.lazy";
 import { getLanguageId } from "@/lib/utils/language";
 import SubmissionResult from "@/components/SubmissionResult/SubmissionResult";
 import { type ISubmissionResultProps } from "@/components/SubmissionResult/SubmissionResult";
-import { Link, useNavigate } from "@tanstack/react-router";
+import { Link } from "@tanstack/react-router";
 // import Submissions from "@/components/Submissions/Submissions";
 
 const SolveProblemPage = () => {
   const authUser = useAuthStore((state) => state.user);
-  const isAuthenticated = useAuthStore((state) => state.isAuthenticated);
   const { id } = Route.useParams();
   const problem = useProblemStore<IProblem | null>((state) => state.problem);
   // const isProblemLoading = useProblemStore<boolean>(
@@ -60,7 +59,6 @@ const SolveProblemPage = () => {
   const getProblemById = useProblemStore((state) => state.getProblemById);
   const isExecuting = useExecuteStore((state) => state.isExecuting);
   const executeProblem = useExecuteStore((state) => state.executeCode);
-  const navigate = useNavigate();
   const [code, setCode] = useState("");
   const [selectedLanguage, setSelectedLanguage] =
     useState<string>("javascript");
@@ -169,10 +167,6 @@ const SolveProblemPage = () => {
     }
   };
 
-  if (!isAuthenticated) {
-    return navigate({ to: "/auth/sign-in" });
-  }
-  
   return (
     <div className="min-h-screen bg-gradient-to-br from-slate-950 via-purple-950 to-indigo-950 flex flex-col">
       {/* Header */}
