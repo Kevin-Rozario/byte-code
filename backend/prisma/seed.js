b6e87faa58de564bddde5310143b34a9b6b48f72
Kevin Rozario <rozariokevin73@gmail.com>
2025-05-30 10:27:31 +0530

    feat: add user creation logic in seed script for database seeding

--- backend/prisma/seed.js
+++ backend/prisma/seed.js
@@ -3,7 +3,19 @@ import { leetcodeProblems } from "../data/problems.js";
 
 async function main() {
   console.log("Start seeding ...");
+  const user = await db.user.create({
+    data: {
+      userName: "dummyadmin",
+      email: "dummy@google.com",
+      password: "dummy@3214",
+      fullName: "Dummy Admin",
+    },
+  });
+
   const problems = await db.problem.createMany({
+    where: {
+      userId: user.id,
+    },
     data: leetcodeProblems,
   });
   console.log(`Created ${problems.count} problems`);
