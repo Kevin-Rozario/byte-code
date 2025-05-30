0d06d19799544cdac562608b6e4f42b81fff8429
Kevin Rozario <rozariokevin73@gmail.com>
2025-05-30 13:40:39 +0530

    chore: remove base and server configuration from Vite setup for cleaner config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,22 +6,11 @@ import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "./",
   plugins: [
     TanStackRouterVite({ target: "react", autoCodeSplitting: true }),
     react(),
     tailwindcss(),
   ],
-  preview: {
-    port: 6435,
-    strictPort: true,
-  },
-  server: {
-    port: 6435,
-    strictPort: true,
-    host: true,
-    origin: "http://0.0.0.0:6435",
-  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
